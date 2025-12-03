eee60489a3cbfb54540e934b30599f05974a120e
Yujiro Juanma <couchenourjuan@gmail.com>
2025-12-02 23:44:45 -0600

    Adding initial config files

---
+++ .config/nvim/lua/plugins/astroui.lua
@@ -0,0 +1,22 @@
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    -- change colorscheme
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    highlights = {
+    },
+    icons = {
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+    },
+  },
+}
